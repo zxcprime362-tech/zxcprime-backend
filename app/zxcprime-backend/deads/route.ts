5955761655a1655cee2bd296eb9ea48ecde36851
iamDaedalus <jerometecson.basc@gmail.com>
2026-02-02 16:20:15 +0800

    34344

--- app/zxcprime-backend/deads/route.ts
+++ app/zxcprime-backend/deads/route.ts
@@ -1,7 +1,7 @@
 import { encodeBase64Url } from "@/lib/base64";
 import { fetchWithTimeout } from "@/lib/fetch-timeout";
 import { NextRequest, NextResponse } from "next/server";
-import { validateBackendToken } from "../0/route";
+import { validateBackendToken } from "@/lib/token";
 
 type Source = {
   file: string;
